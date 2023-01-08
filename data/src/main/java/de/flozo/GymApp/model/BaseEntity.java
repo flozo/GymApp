7ad9ca3cb9f51ff5e2faa10ec3a9101936823e42
flozo.home <github.mail@flozo.de>
2023-01-08 15:20:27 +0100

    Add entity Muscle

--- data/src/main/java/de/flozo/GymApp/model/BaseEntity.java
+++ data/src/main/java/de/flozo/GymApp/model/BaseEntity.java
@@ -19,5 +19,5 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
 }
---
+++ data/src/main/java/de/flozo/GymApp/model/Muscle.java
@@ -0,0 +1,18 @@
+package de.flozo.GymApp.model;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class Muscle extends BaseEntity {
+
+    private String scientificName;
+    private String shortName;
+    private String part;
+
+}
