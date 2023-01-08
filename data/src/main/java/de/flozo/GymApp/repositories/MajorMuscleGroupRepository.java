af5371e0851dc4b49d91c303621f023ee8baefee
flozo.home <github.mail@flozo.de>
2023-01-08 16:03:39 +0100

    Add MajorMuscleGroupRepository and MajorMuscleGroupService

---
+++ data/src/main/java/de/flozo/GymApp/repositories/MajorMuscleGroupRepository.java
@@ -0,0 +1,8 @@
+package de.flozo.GymApp.repositories;
+
+import de.flozo.GymApp.model.MajorMuscleGroup;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MajorMuscleGroupRepository extends CrudRepository<MajorMuscleGroup, Long> {
+
+}
---
+++ data/src/main/java/de/flozo/GymApp/services/MajorMuscleGroupService.java
@@ -0,0 +1,8 @@
+package de.flozo.GymApp.services;
+
+import de.flozo.GymApp.model.MajorMuscleGroup;
+
+public interface MajorMuscleGroupService extends CrudService<MajorMuscleGroup, Long> {
+
+
+}
