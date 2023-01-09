43ae19df2b0a1f762328c586b4d59f4504e904d7
flozo.home <github.mail@flozo.de>
2023-01-09 17:23:04 +0100

    Add index controller

---
+++ web/src/main/java/de/flozo/GymApp/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package de.flozo.GymApp.controllers;
+
+import de.flozo.GymApp.services.WorkoutUnitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    private final WorkoutUnitService workoutUnitService;
+
+    public IndexController(WorkoutUnitService workoutUnitService) {
+        this.workoutUnitService = workoutUnitService;
+    }
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String getIndexPage(Model model) {
+        model.addAttribute("workout_units", workoutUnitService.findAll());
+        return "index";
+    }
+
+}
