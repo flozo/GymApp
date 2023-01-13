867046b1cce640fc0e851fa4ff67c67705a53d75
flozo.home <github.mail@flozo.de>
2023-01-13 16:55:55 +0100

    Add methods for adding exercises where muscle is primary/secondary

--- data/src/main/java/de/flozo/GymApp/model/Muscle.java
+++ data/src/main/java/de/flozo/GymApp/model/Muscle.java
@@ -3,6 +3,7 @@ package de.flozo.GymApp.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -23,9 +24,26 @@ public class Muscle extends BaseEntity {
 
 
     @ManyToMany(mappedBy = "primaryMusclesWorked")
-    private Set<Exercise> exercisesPrimaryMuscle;
+    private Set<Exercise> exercisesPrimaryMuscle = new HashSet<>();
 
     @ManyToMany(mappedBy = "secondaryMusclesWorked")
-    private Set<Exercise> exercisesSecondaryMuscle;
+    private Set<Exercise> exercisesSecondaryMuscle = new HashSet<>();
+
+
+    public Muscle addExercisePrimaryMuscle(Exercise exercise) {
+        exercisesPrimaryMuscle.add(exercise);
+        if (!exercise.getPrimaryMusclesWorked().contains(this)) {
+            exercise.addPrimaryMuscleWorked(this);
+        }
+        return this;
+    }
+
+    public Muscle addExerciseSecondaryMuscle(Exercise exercise) {
+        exercisesSecondaryMuscle.add(exercise);
+        if (!exercise.getSecondaryMusclesWorked().contains(this)) {
+            exercise.addSecondaryMuscleWorked(this);
+        }
+        return this;
+    }
 
 }
