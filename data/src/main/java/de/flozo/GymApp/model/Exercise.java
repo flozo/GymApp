46db4151c48511de58e651a28d75da360ee4ab85
flozo.home <github.mail@flozo.de>
2023-01-13 16:55:06 +0100

    Add methods for adding primary and secondary muscles worked

--- data/src/main/java/de/flozo/GymApp/model/Exercise.java
+++ data/src/main/java/de/flozo/GymApp/model/Exercise.java
@@ -3,6 +3,7 @@ package de.flozo.GymApp.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Getter
@@ -18,15 +19,32 @@ public class Exercise extends BaseEntity {
 
     @ManyToMany
     @JoinTable(name = "exercise_primary_muscles",
-    joinColumns = @JoinColumn(name = "exercise_id"),
-    inverseJoinColumns = @JoinColumn(name = "muscle_id"))
-    private Set<Muscle> primaryMusclesWorked;
+            joinColumns = @JoinColumn(name = "exercise_id"),
+            inverseJoinColumns = @JoinColumn(name = "muscle_id"))
+    private Set<Muscle> primaryMusclesWorked = new HashSet<>();
 
     @ManyToMany
     @JoinTable(name = "exercise_secondary_muscles",
             joinColumns = @JoinColumn(name = "exercise_id"),
             inverseJoinColumns = @JoinColumn(name = "muscle_id"))
-    private Set<Muscle> secondaryMusclesWorked;
+    private Set<Muscle> secondaryMusclesWorked = new HashSet<>();
+
+
+    public Exercise addPrimaryMuscleWorked(Muscle muscle) {
+        primaryMusclesWorked.add(muscle);
+        if (!muscle.getExercisesPrimaryMuscle().contains(this)) {
+            muscle.addExercisePrimaryMuscle(this);
+        }
+        return this;
+    }
+
+    public Exercise addSecondaryMuscleWorked(Muscle muscle) {
+        secondaryMusclesWorked.add(muscle);
+        if (!muscle.getExercisesSecondaryMuscle().contains(this)) {
+            muscle.addExerciseSecondaryMuscle(this);
+        }
+        return this;
+    }
 
 
 }
